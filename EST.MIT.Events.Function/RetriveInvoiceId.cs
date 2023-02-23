8bfddba7a4c956ec8d6a2966943758910c194b96
Street, Samantha <Samantha.Street@defra.gov.uk>
2023-02-23 17:04:41 +0000

    Adding event type

--- EST.MIT.Events.Function/RetriveInvoiceId.cs
+++ EST.MIT.Events.Function/RetriveInvoiceId.cs
@@ -19,17 +19,14 @@ namespace MIT.Events.Function
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string name = req.Query["eventType"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            
+            return new OkResult();
+          
         }
     }
 }
