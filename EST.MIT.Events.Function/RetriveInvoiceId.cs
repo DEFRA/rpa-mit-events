ba746fe24b99c84fb82b08965653b6fc3a5e5bd4
Street, Samantha <Samantha.Street@defra.gov.uk>
2023-02-23 17:45:45 +0000

    trying again

--- EST.MIT.Events.Function/RetriveInvoiceId.cs
+++ EST.MIT.Events.Function/RetriveInvoiceId.cs
@@ -25,14 +25,7 @@ namespace MIT.Events.Function
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-
-            string responseMessage = string.IsNullOrEmpty(name)
-               ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-               : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-           
-
+            return new OkResult();
         }
     }
 }
