ddbac343ae5221571b6f3ecbd8336b2a1e20d8be
David Baker <daveonplanetearth@gmail.com>
2023-02-28 10:52:54 +0000

    Example azure function xUnit test.

---
+++ EST.MIT.Events.Function.Test/NullScope.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EST.MIT.Events.Function.Test
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+
+        private NullScope() { }
+
+        public void Dispose() { }
+    }
+
+}
