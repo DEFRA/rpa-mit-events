925511889a2edcf371d56cebb22f6c25fbf0a954
Street, Samantha <Samantha.Street@defra.gov.uk>
2023-02-28 08:55:26 +0000

    trying to write the test

---
+++ EST.MIT.Events.Function.Test/RetriveInvoiceIdTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using EST.MIT.Events.Function;
+
+namespace EST.MIT.Events.Function.Test
+{
+    public class RetriveInvoiceIdTest
+    {
+        [Fact]
+        public void InvoiceIdIsRetrived()
+        {
+            int number = 5;
+
+            var invoiceId = new invoiceId(number);
+            var eventType = new req.Query["eventType"];
+
+            Assert.Equal()
+
+        }
+
+
+    }
+}
